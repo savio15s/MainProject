3fe23fa2ab52782f0f6ba2387cd8287da9fef943
bashabhai <super@gmail.com>
2021-07-06 23:14:04 +0530

    history

--- MyReact/proj05-routes/src/components/Home.js
+++ MyReact/proj05-routes/src/components/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
-  
-function Home (){
-    return <h1>Welcome to the world of React!</h1>
+import { useHistory } from 'react-router-dom';
+
+function Home() {
+    const history = useHistory();
+    return (
+        <>
+            <h1>Welcome to the world of React!</h1>
+            <button onClick={() => history.push('/contact')}>Go TO</button>
+        </>
+    );
 }
-  
+
 export default Home;
